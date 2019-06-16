4049e938ae77d0455f03b4b0070621b0fbcd5849
AmanyAts <amanyalattass@gmail.com>
2019-06-16 12:46:08 +0300

    Updating

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,6 +1,7 @@
 class PlansController < ApplicationController
     before_action :set_plan, only: [:show, :edit, :update, :destroy]
     def index
+        @plan = current_user.plans.new #add new action here
         @plans = current_user.plans
     end
 
@@ -11,16 +12,38 @@ class PlansController < ApplicationController
         @plan = current_user.plans.new
     end
 
+    # GET /books/1/edit
+    def edit
+    end
+
 
     def create
         @plan = current_user.plans.new(plan_params)
     
         if @plan.save
-            redirect_to plan_path(@plan)        
+            redirect_to plans_path()#some change here        
         else
             render :new
         end
       end
+
+      def update
+        @plan = current_user.plans.new(plan_params)
+       
+        if @plan.update(plan_params)
+          rredirect_to plans_path()
+        else
+          render 'edit'
+        end
+      end
+
+      
+      def destroy
+       
+        @plan.destroy
+    
+        redirect_to plans_path()
+       end
 
     def set_plan
         @plan = current_user.plans.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  
+  get "/home", to:"home#index"
+  root  "home#index"
+  
   devise_for :users
   resources :plans
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  
-  get "/home", to:"home#index"
-  root  "home#index"
+ 
   
 end
