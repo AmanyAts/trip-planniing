27a5a1f345093dccf4d8eea940f4fcc992e7e20e
AmanyAts <amanyalattass@gmail.com>
2019-06-19 16:09:04 +0300

    Background Pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # has_many :plans
-    has_many :destinations
+    has_many :destinations, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
